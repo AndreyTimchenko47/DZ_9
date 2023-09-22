1616ab05f62a1ec388b680eaf951f7c09f022a12
Andrey <Andrey@mail.ru>
2023-09-22 18:10:00 +0300

    DZ_9

---
+++ Task66/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputeIntNumber(string message)
+{
+    int number = 0;
+    Console.Write(message);
+    number = Convert.ToInt32(Console.ReadLine());
+
+    while (number < 1)
+    {
+        System.Console.WriteLine("Некорректный ввод!");
+        Console.Write(message);
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
+
+int SumNatural(int num1, int num2)
+{
+    int result = num1;
+    if (num1 == num2)
+        return 1;
+    if (num1 > num2)
+    {
+        num2++;
+        result = num2 + SumNatural(num2, num1);
+        return result;
+    }
+    else
+    {
+        num1++;
+        result = num1 + SumNatural(num1, num2);
+        return result;
+    }
+}
+
+Console.WriteLine("Введите два натуральных положительных числа");
+int number1 = InputeIntNumber("Введите 1-е натуральное положительное число: ");
+int number2 = InputeIntNumber("Введите 2-е натуральное положительное число: ");
+
+int sumNumbers = SumNatural(number1, number2);
+Console.WriteLine($"Сумма натуральных положительных чисел от {number1} до {number2} = {sumNumbers}");
---
+++ Task68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputeIntNumber(string message)
+{
+    int number = 0;
+    Console.Write(message);
+    number = Convert.ToInt32(Console.ReadLine());
+
+    while (number < 0)
+    {
+        System.Console.WriteLine("Некорректный ввод! Число должно быть неотрицательным");
+        Console.Write(message);
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
+
+int AckermannFunc(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else 
+    if (num2 == 0 && num1 > 0) return AckermannFunc(num1 - 1, 1);
+    else 
+    return (AckermannFunc(num1 - 1, AckermannFunc(num1, num2 - 1)));
+}
+
+Console.WriteLine("Введите два числа");
+int number1 = InputeIntNumber("Введите 1-е положительное число: ");
+int number2 = InputeIntNumber("Введите 2-е положительное число: ");
+int ackermannNumber = AckermannFunc(number1, number2);
+Console.WriteLine($"Число Аккермана составляет: {ackermannNumber}");
